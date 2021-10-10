a398eb73896f0cb5df9ff017d155c376dfcc0a6b
jorrmaglione <jorr.hs@hotmail.com>
2021-10-09 22:01:21 -0300

    Crear clase conexion y agregar archivo de configuracion de acceso a db en .gitignore

---
+++ digesto/models/Connection.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace models;
+
+use Exception;
+
+/**
+ * Class Connection
+ *
+ * @package models
+ */
+class Connection {
+    /**
+     * @var resource
+     */
+    private static $conn = null;
+
+    /**
+     * Connection constructor.
+     */
+    private function __construct() { }
+
+    /**
+     * @return string
+     */
+    private static function getConnectionString(): string {
+        $arr = explode(';', file_get_contents('config/db-config'));
+        return sprintf("host=%s user=%s password=%s dbname=%s", $arr[0], $arr[1], $arr[2], $arr[3]);
+    }
+
+    /**
+     * @return resource
+     * @throws Exception
+     */
+    public static function getConnection() {
+        if (self::$conn === null) {
+            self::$conn = pg_connect(self::getConnectionString());
+            if (self::$conn === false)
+                throw new Exception('Cannot connect to the database');
+        }
+        return self::$conn;
+    }
+}
