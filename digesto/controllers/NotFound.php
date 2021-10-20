0449cdbadef8bcca80505a3935b927da89e9e6f3
jorrmaglione <jorr.hs@hotmail.com>
2021-10-20 12:18:54 -0300

    Agregar header de error 404 y cambiar color de fondo

--- digesto/controllers/NotFound.php
+++ digesto/controllers/NotFound.php
@@ -12,6 +12,7 @@ abstract class NotFound {
      *
      */
     public static function index(): void {
+        header('HTTP/1.0 404 Not Found');
         echo file_get_contents('src/pages/not-found/not-found.html');
     }
 }
