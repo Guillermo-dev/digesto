19d208619be2af2e816884970f8c23b03f2eabe8
Pao <paosantii@gmail.com>
2021-11-01 19:29:13 -0300

    cambios insignificantes

--- digesto/routes/web.php
+++ digesto/routes/web.php
@@ -5,6 +5,7 @@
  */
 if (isset($router)) {
     $router->get('/', 'controllers\Home@index');
+    $router->get('/documentos/{id}', 'controllers\Home@documento');
 
     $router->set404('controllers\NotFound@index');
 }
