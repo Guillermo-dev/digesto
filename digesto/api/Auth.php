ab4ab7a18cbdef3f742a9b61855e2122148654b0
GuillermoTejera <guilletejera@gmail.com>
2021-11-01 23:51:03 -0300

    Se implemento el login con google y manejo de sesiones

--- digesto/api/Auth.php
+++ digesto/api/Auth.php
@@ -4,6 +4,11 @@ namespace api;
 
 use Exception;
 use helpers\Response;
+use helpers\Request;
+use models\Usuario;
+use Google_Client;
+
+
 
 /**
  * Class Auth
@@ -18,17 +23,29 @@ abstract class Auth {
     public static function login(): void {
         if (isset($_SESSION['user']))
             throw new Exception('Forbidden', 403);
+        
+        $data = Request::getBodyAsJson();
+        $google_client = new Google_Client(['client_id' => '471191857447-594hnuasrs49qe51amgma6o0hiir07de.apps.googleusercontent.com']);
 
-        //Sign in logic
+        $payload = $google_client->verifyIdToken($data->gToken);
+        if (!$payload)
+            throw new Exception();
 
-        throw new Exception('Not implemented', 503);
+        $usuario = Usuario::getUsuarioByEmail($payload["email"]);
+        if (!$usuario)
+            throw new Exception();
+
+        $_SESSION['user'] = serialize($usuario);
+
+        Response::getResponse()->setStatus('success');
     }
 
     /**
      *
      * @throws Exception
      */
-    public static function logout(): void {
+    public static function logout(): void
+    {
         if (!isset($_SESSION['user']))
             throw new Exception('Forbidden', 403);
 
