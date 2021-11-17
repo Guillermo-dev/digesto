fb7e23c452dc23854cf9096646726505e6d5d2a1
Pao <paosantii@gmail.com>
2021-11-17 01:32:02 -0300

    nuevaVista

--- digesto/src/components/AdminNewDocumento.js
+++ digesto/src/components/AdminNewDocumento.js
@@ -1,5 +1,5 @@
-import {createElement, createStyle} from "../global/js/util.js";
-import {Component} from "./Component.js";
+import { createElement, createStyle } from "../global/js/util.js";
+import { Component } from "./Component.js";
 
 // language=CSS
 createStyle()._content(`
@@ -10,7 +10,6 @@ createStyle()._content(`
     }
 
     .cuerpoDoc {
-        padding: 20px 0 0 0;
         width: auto;
         margin: auto;
         overflow: hidden;
@@ -44,6 +43,18 @@ createStyle()._content(`
         padding: 10px 10px 10px 10px;
     }
 
+    .contenedor {
+        padding: 10px 10px 10px 10px;
+        overflow: hidden;
+        box-shadow: 0 4px 4px 1px #18363d33;
+        width: auto;
+        height: auto;
+    }
+
+    .cargaDocumento{
+        display: flex;
+    }
+
 `);
 
 /**
@@ -56,8 +67,101 @@ export default function AdminNewDocumento() {
     this.root = createElement("div")._class("AdminNewDocumento")._html(` 
     <!--Loaded-->
     <div class="container css-loaded">
-        <!--[PAO] [PAO] [PAO] [PAO] ACA VA EL PINCHE ACHE-TE-EME-ELE (HTML) [PAO] [PAO] [PAO] [PAO]-->
-        <h1 class="display-1">PAO, ACA VA EL CODIGO, POR FAVORE!</h1>
+        <div class="cuerpoDoc mt-4 border shadow mb-5 bg-white  rounded-top" id="cuerpoDoc">
+            <div class="encabezadoDoc " id="encabezadoDoc">
+                <h3> Detalles del Documento</h3>
+            </div>          
+            <div class="contenedor">
+                <div class="row">
+                    <div class="col-sm>
+                        <form>
+                            <div class="form-group">
+                                <label class="fw-bold mt-2" for="titulo">Título*</label>
+                                <input type="text " class="form-control " id="titulo " placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label class="fw-bold mt-2" for="descripcion">Descripción</label>
+                                <textarea  class="form-control " id="descripcion " placeholder=""> </textarea>
+                            </div>
+                            <div class="form-group ">
+                                <label class="fw-bold mt-2" for="numero">Número*</label>
+                                <input type="text " class="form-control " id="numero " placeholder="">
+                            </div>
+                            <div class="form-group ">
+                                <label class="fw-bold mt-2" for="campaña">Fecha*</label>
+                                <input type="fate" class="form-control " id="fecha " placeholder="">
+                            </div> 
+                            <div class="form-group ">
+                                <label class="fw-bold mt-2" for="campaña">Tipo*</label>
+                                <input type="text" class="form-control " id="tipo " placeholder="">
+                            </div> 
+                            <div class="form-group  ">
+                                <label class="fw-bold mt-2" for="campaña">Emisor*</p></label>
+                                <select class="form-control form-control-sm p-2">
+                                    <option>Rector</option>
+                                    <option>Consejo Superior</option>
+                                    <option>Otros</option>
+                                </select>
+                            <div class="form-group ">
+                                <label class="fw-bold mt-2" for="campaña">Etiquetas*</label>
+                                <input type="text " class="form-control " id="etiquetas " placeholder="">
+                                <p>Se recomienda agregar entre 5 y 8 etiquetas por documento. Usar palabras representativas.</p>
+                            </div> 
+
+                            <div class="row d-flex">
+                                <div class="col-ms">
+                                    <div class="form-check form-check-inline">
+                                        <label class="fw-bold mt-2" for="privacidad">Privacidad*</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="publico" id="publico" value="publico" checked>
+                                        <label class="form-check-label" for="publico"> Público </label>                  
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="Privado" id="Privado" value="Privado">
+                                        <label class="form-check-label" for="Privado"> Privado </label>                   
+                                    </div>
+                                </div>
+                               
+                                <div class="col-ms">
+                                    <div class="form-check form-check-inline">
+                                        <label class="fw-bold mt-2" for="Estado">Estado*</label>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="Descargable" id="Descargable" value="Descargable" checked>
+                                            <label class="form-check-label" for="Descargable">Descargable </label>               
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="No descargable" id="No descargable" value="No descargable">
+                                            <label class="form-check-label" for="No descargable">No descargable</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="cargaDocumento d-grid">
+                                <div class="card text-center mt-3">
+                                    <div class="card-header fw-bold  bg-secondary bg-opacity-25 ">
+                                        Cargar Documento*
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="card-text">Seleccione el archivo que desea subir</p>
+                                        <input type= "file" name="Upload" >
+                                    </div>
+                                </div>   
+                            </div>
+                            <div class="text-end mt-2 ">
+                                <button type="submit " class="btn btn-primary">Cancelar</button>
+                                <button type="submit " class="btn btn-primary">Guardar</button> 
+                            </div>
+                        </form>
+                    </div>                           
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+
     </div>
     <!--Error-->
     <div class="css-error p-3 text-center h-100 d-flex justify-content-center align-items-center flex-column">
